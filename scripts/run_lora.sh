1973f909bc59835074ed3fc166bf8ed95b373345
Chris <144029412+ByteSculptor07@users.noreply.github.com>
2025-04-09 22:26:36 +0200

    Update run_lora.sh

--- scripts/run_lora.sh
+++ scripts/run_lora.sh
@@ -21,6 +21,6 @@ python train.py --load_model $load_model \
 --ctx_len $ctx_len --micro_bsz $micro_bsz \
 --epoch_steps $epoch_steps --epoch_count 1 --epoch_begin 0 --epoch_save $epoch_save \
 --lr_init 2e-5 --lr_final 2e-5 --warmup_steps 0 --beta1 0.9 --beta2 0.99 --adam_eps 1e-8 \
---accelerator gpu --devices 1 --precision fp16 --strategy deepspeed_stage_1 --grad_cp 1 \
+--accelerator gpu --devices 1 --precision fp16 --strategy deepspeed_stage_1 --grad_cp 0 \
 --my_testing "x070" \
 --peft lora --lora_config $lora_config
